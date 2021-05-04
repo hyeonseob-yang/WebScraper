75da3d73032ed4da36a8d2f82516952931a57b1a
Hyeonseob Yang <nick_yang_2@sfu.ca>
2021-05-03 19:20:55 -0700

    Add MySql database connector

---
+++ WebScraper/Connector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace WebScraper {
+    class Connector {
+        public static void Connect() {
+            string connStr = "server=localhost;user=root;database=world;port=3306;password=test";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+            } catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+    }
+}
--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -13,7 +13,8 @@ namespace WebScraper {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
+            Connector.Connect();
         }
     }
 }
