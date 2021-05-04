735d5a57654a04246843c8ca711bfd67df37c72b
Hyeonseob Yang <nick_yang_2@sfu.ca>
2021-05-03 19:20:29 -0700

    Add scraping functionality in application

---
+++ WebScraper/MainWindow.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace WebScraper {
+    partial class MainWindow {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.rtb_debugDisplay = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtb_debugDisplay
+            // 
+            this.rtb_debugDisplay.Location = new System.Drawing.Point(12, 12);
+            this.rtb_debugDisplay.Name = "rtb_debugDisplay";
+            this.rtb_debugDisplay.Size = new System.Drawing.Size(776, 397);
+            this.rtb_debugDisplay.TabIndex = 1;
+            this.rtb_debugDisplay.Text = "";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtb_debugDisplay);
+            this.Controls.Add(this.button1);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rtb_debugDisplay;
+    }
+}
+
---
+++ WebScraper/MainWindow.cs
@@ -0,0 +1,86 @@
+﻿// Base code from:
+// https://dev.to/rachelsoderberg/create-a-simple-web-scraper-in-c-1l1m
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using AngleSharp.Text;
+
+namespace WebScraper {
+    public partial class MainWindow : Form {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private string Title { get; set; }
+        private string Url { get; set; }
+        private string siteUrl = "https://www.oceannetworks.ca/news/stories";
+        private string[] QueryTerms { get; } = { "Ocean", "Nature", "Pollution" };
+
+        internal async void ScrapeWebsite() {
+            CancellationTokenSource cancellationToken = new CancellationTokenSource();
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage request = await httpClient.GetAsync(siteUrl);
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            Stream response = await request.Content.ReadAsStreamAsync();
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            HtmlParser parser = new HtmlParser();
+            IHtmlDocument document = parser.ParseDocument(response);
+
+            GetScrapeResults(document);
+        }
+
+        private void GetScrapeResults(IHtmlDocument document) {
+            IEnumerable<IElement> articleLink;
+
+            foreach (var term in QueryTerms) {
+                articleLink = document.All.Where(x => x.ClassName == "views-field views-field-nothing"
+                && (x.ParentElement.InnerHtml.Contains(term)
+                || x.ParentElement.InnerHtml.Contains(term.ToLower()))).Skip(1);
+
+                if (articleLink.Any()) {
+                    PrintResults(articleLink);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            ScrapeWebsite();
+        }
+
+        public void PrintResults(IEnumerable<IElement> articleLink) {
+            foreach (var element in articleLink) {
+                CleanUpResults(element);
+                rtb_debugDisplay.AppendText($"{Title} - {Url}{Environment.NewLine}");
+            }
+        }
+
+        private void CleanUpResults(IElement results) {
+            string htmlResult = results.InnerHtml.ReplaceFirst("    <span class=\"field-content\"><div><a href=\"", "https://www.oceannetworks.ca");
+            htmlResult = htmlResult.ReplaceFirst("\">", "*");
+            htmlResult = htmlResult.ReplaceFirst("</a></div>\n<div class=\"article-title-top\">", "-");
+            htmlResult = htmlResult.ReplaceFirst("</div>\n<hr></span> ", "");
+
+            SplitResults(htmlResult);
+        }
+
+        private void SplitResults(string htmlResult) {
+            string[] splitResults = htmlResult.Split('*');
+            Url = splitResults[0];
+            Title = splitResults[1];
+        }
+    }
+}
